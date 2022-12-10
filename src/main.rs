54071d90b81a8c02c5667d70a0054610a3a70632
anderskloborg <anderskloborg@gmail.com>
2022-12-11 00:27:20 +0100

    laid groundwork for solving day 7.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 #![feature(let_chains)]
 #![feature(array_windows)]
 
+mod day10;
 mod day6;
+mod day7;
 mod day9;
 
 fn main() {
-    let result = crate::day9::solution::part2();
+    let result = crate::day10::solution::part1();
 
     println!("{:?}", result);
 }
