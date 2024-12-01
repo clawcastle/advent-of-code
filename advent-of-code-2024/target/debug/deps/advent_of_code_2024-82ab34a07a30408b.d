a7f766e644f161445fc2cafd4c62a414dda605bf
clawcastle <anderskloborg@gmail.com>
2024-12-01 20:02:13 +0100

    Add proper gitignore.

--- advent-of-code-2024/target/debug/deps/advent_of_code_2024-82ab34a07a30408b.d
+++
@@ -1,6 +0,0 @@
-/home/clawcastle/repos/advent-of-code/advent-of-code-2024/target/debug/deps/libadvent_of_code_2024-82ab34a07a30408b.rmeta: src/main.rs src/day1.rs
-
-/home/clawcastle/repos/advent-of-code/advent-of-code-2024/target/debug/deps/advent_of_code_2024-82ab34a07a30408b.d: src/main.rs src/day1.rs
-
-src/main.rs:
-src/day1.rs:
--- advent-of-code-2024/target/debug/deps/iter_tools-a138c55810561d3b.d
+++
@@ -1,9 +0,0 @@
-/home/clawcastle/repos/advent-of-code/advent-of-code-2024/target/debug/deps/libiter_tools-a138c55810561d3b.rmeta: /home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/src/lib.rs /home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/src/iter.rs /home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/Readme.md
-
-/home/clawcastle/repos/advent-of-code/advent-of-code-2024/target/debug/deps/iter_tools-a138c55810561d3b.d: /home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/src/lib.rs /home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/src/iter.rs /home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/Readme.md
-
-/home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/src/lib.rs:
-/home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/src/iter.rs:
-/home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0/Readme.md:
-
-# env-dep:CARGO_MANIFEST_DIR=/home/clawcastle/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iter_tools-0.24.0
